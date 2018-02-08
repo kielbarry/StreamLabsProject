b59c126db26f13230a4a57706200211ef25743b1
Kiel Barry <kielbarry@Kiels-MacBook-Pro.local>
2018-02-07 17:30:31 -0800

    googauth

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,10 @@
     </g-signin-button>
  -->
     <div class="g-signin2" data-onsuccess="onSignIn" data-onerror="onSignInError"></div>
+
+  <button  v-on:click="signOut"
+  ></button>
+
   </div>
 </template>
 
@@ -25,13 +29,18 @@ export default {
   },
   methods: {
     onSignIn(googleUser){
-      console.log("HEREEEEEEE")
+      console.log("HEREEEEEEE", googleUser)
       const profile = googleUser.getBasicProfile() // etc etc
       console.log(profile)
     },
     onSignInError(err) {
       console.log("here is the ERRORRRRRRR", err)
     },
+    signOut() {
+      gapi.auth2.getAuthInstance().disconnect();
+    },
+
+
     onSignInSuccess (googleUser) {
       // `googleUser` is the GoogleUser object that represents the just-signed-in user.
       // See https://developers.google.com/identity/sign-in/web/reference#users
